5ddebc57a74eaa795c1ca6d1c78ed94b346ccdd0
Lvnge <roy9096@hotmail.com>
2026-06-27 03:42:18 -0600

    add project pages

--- app/page.jsx
+++ app/page.jsx
@@ -8,6 +8,8 @@ export default function Home() {
   const taker = portfolioItems.find((i) => i.slug === "taker");
   const sendas = portfolioItems.find((i) => i.slug === "sendas");
   const despues = portfolioItems.find((i) => i.slug === "despues-de-la-senda");
+  const eda = portfolioItems.find((i) => i.slug === "ejercicios-de-amor");
+
   return (
     <PageWrapper>
       <main>
@@ -27,33 +29,44 @@ export default function Home() {
             details={taker.details}
           />
         </div>
-        <div>
-          <ProjectCard
-            title={sendas.title}
-            type={sendas.type}
-            href="/portfolio/sendas"
-            description={sendas.description}
-            spreadDivider={false}
-            aspectRatio={sendas.aspectRatio}
-            images={sendas.images}
-            details={sendas.details}
-            singleImage={true}
-          />
-        </div>
-        <div>
-          <ProjectCard
-            title={despues.title}
-            type={despues.type}
-            href="/portfolio/despues-de-la-senda"
-            description={despues.description}
-            spreadDivider={false}
-            aspectRatio={despues.aspectRatio}
-            images={despues.images}
-            details={despues.details}
-            singleImage={false}
-          />
-        </div>
-        <div>
+
+        <ProjectCard
+          title={sendas.title}
+          type={sendas.type}
+          href="/portfolio/sendas"
+          description={sendas.description}
+          spreadDivider={false}
+          aspectRatio={sendas.aspectRatio}
+          images={sendas.images}
+          details={sendas.details}
+          singleImage={true}
+        />
+
+        <ProjectCard
+          title={despues.title}
+          type={despues.type}
+          href="/portfolio/despues-de-la-senda"
+          description={despues.description}
+          spreadDivider={false}
+          aspectRatio={despues.aspectRatio}
+          images={despues.images}
+          details={despues.details}
+          singleImage={false}
+        />
+
+        <ProjectCard
+          title={eda.title}
+          type={eda.type}
+          href="/portfolio/ejercicios-de-amor"
+          description={eda.description}
+          singleImage={eda.singleImage}
+          aspectRatio={eda.aspectRatio}
+          images={eda.images}
+          details={eda.details}
+        />
+        <div
+          style={{ width: "100%", display: "flex", justifyContent: "center" }}
+        >
           <Player src="/audio/one-last-kiss.mp3" title="One last kiss" />
         </div>
       </main>
